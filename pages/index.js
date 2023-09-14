752c1ca0fa42e45dc37f72862eb0996353850195
parampreet singh <parampreetsingh1092@gmail.com>
2023-09-14 22:46:07 +0530

    components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-
 import Navbar from "components/Navbar";
 import ResourceHighlight from "components/ResourceHighlight";
 import Newsletter from "components/Newsletter";
 import ResourceList from "components/ResourceList";
 import Footer from "components/Footer";
+//commenting for github streak
 
 function Home() {
 
