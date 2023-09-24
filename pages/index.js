d2d11a29c4288601b8a3d8c366bf2af13b46dff0
parampreet singh <parampreetsingh1092@gmail.com>
2023-09-24 16:38:27 +0530

    data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,48 @@
-import React from "react";
 import Layout from "components/Layout";
 import ResourceHighlight from "components/ResourceHighlight";
 import Newsletter from "components/Newsletter";
 import ResourceList from "components/ResourceList";
 import Footer from "components/Footer";
-function Home(resources) {
+import data from "pages/api/data.json"
+
+function Home({resources}) {
   return (
     <Layout>
-      <ResourceHighlight 
-        resources={resources.slice(0,2)}
+      <ResourceHighlight
+        resources={resources.slice(0, 2)}
       />
       <Newsletter />
-      <ResourceList 
+      <ResourceList
         resources={resources.slice(2)}
       />
       <Footer />
     </Layout>
   )
 }
+// is called everytime you will visit the page 
+// function is executed on the server
+// export async function getServerSideProps() {
 
-//get statis props function is from next js and is handled on the server itself of next
-export  async function getStaticProps(){
+//   const resData = await fetch("http://localhost:3000/api/resources");
+//   const data = await resData.json();
 
-  const resdata =  await fetch("http://localhost:3000/api/resources"); 
-  const data = await resdata.json();
-  return{
-    props:{
-     resources : data
+//   return {
+//     props: {
+//       resources: data
+//     }
+//   }
+// }
+
+// is called at the build time and its called only once
+export async function getStaticProps() {
+
+
+  return {
+    props: {
+      resources: data
     }
   }
 }
+
+
 export default Home;
