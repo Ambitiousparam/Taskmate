32c396a08c8aaf2589fd673b6c007cfc8457bfdf
parampreet singh <parampreetsingh1092@gmail.com>
2023-09-25 11:32:16 +0530

    data fetching from another application

--- pages/index.js
+++ pages/index.js
@@ -3,46 +3,32 @@ import ResourceHighlight from "components/ResourceHighlight";
 import Newsletter from "components/Newsletter";
 import ResourceList from "components/ResourceList";
 import Footer from "components/Footer";
-import data from "pages/api/data.json"
+import { useEffect } from "react";
 
-function Home({resources}) {
+function Home({ resources }) {
+  useEffect(() => { 
+             fetch("http://localhost:3001/api/resources");
+  }, []);
+
   return (
     <Layout>
-      <ResourceHighlight
-        resources={resources.slice(0, 2)}
-      />
+      <ResourceHighlight resources={resources.slice(0, 2)} />
       <Newsletter />
-      <ResourceList
-        resources={resources.slice(2)}
-      />
+      <ResourceList resources={resources.slice(2)} />
       <Footer />
     </Layout>
-  )
+  );
 }
-// is called everytime you will visit the page 
-// function is executed on the server
-// export async function getServerSideProps() {
 
-//   const resData = await fetch("http://localhost:3000/api/resources");
-//   const data = await resData.json();
-
-//   return {
-//     props: {
-//       resources: data
-//     }
-//   }
-// }
-
-// is called at the build time and its called only once
-export async function getStaticProps() {
-
-
+export async function getServerSideProps() {
+  const resData = await fetch("http://localhost:3001/api/resources");
+  const data = await resData.json();
+  console.log(data);
   return {
     props: {
       resources: data
     }
-  }
+  };
 }
-
 
 export default Home;
