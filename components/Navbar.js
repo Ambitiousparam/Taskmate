0f1964790cad2e0a52b0484acb571f183af1c1f0
parampreet singh <parampreetsingh1092@gmail.com>
2023-10-02 19:00:48 +0530

    navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,15 @@
-const Navbar = () => {
+import Link from "next/link";
 
+const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="container">
         <div className="navbar-brand">
-          <a className="navbar-item" href="../">
-            <h1>Content Manager</h1>
-          </a>
+          <Link href="/">
+            <a className="navbar-item">
+              <h1>Content Manager</h1>
+            </a>
+          </Link>
           <span className="navbar-burger burger" data-target="navbarMenu">
             <span></span>
             <span></span>
@@ -15,7 +18,7 @@ const Navbar = () => {
         </div>
         <div id="navbarMenu" className="navbar-menu">
           <div className="navbar-end">
-            <div className=" navbar-item">
+            <div className="navbar-item">
               <div className="control has-icons-left">
                 <input className="input is-rounded" type="email" placeholder="Search" />
                 <span className="icon is-left">
@@ -23,12 +26,16 @@ const Navbar = () => {
                 </span>
               </div>
             </div>
-            <a className="navbar-item is-active is-size-5 has-text-weight-semibold">
-              Home
-            </a>
-            <a className="navbar-item is-size-5 has-text-weight-semibold">
-              Examples
-            </a>
+            <Link href="/">
+              <a className="navbar-item is-active is-size-5 has-text-weight-semibold">
+                Home
+              </a>
+            </Link>
+            <Link href="/resources/new">
+              <a className="navbar-item is-size-5 has-text-weight-semibold">
+                Add
+              </a>
+            </Link>
             <a className="navbar-item is-size-5 has-text-weight-semibold">
               Features
             </a>
@@ -36,6 +43,7 @@ const Navbar = () => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
+
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,10 @@ import ResourceHighlight from "components/ResourceHighlight";
 import Newsletter from "components/Newsletter";
 import ResourceList from "components/ResourceList";
 import Footer from "components/Footer";
-import { useEffect } from "react";
 
 // CORS
+
 function Home({resources}) {
-
-  useEffect(() => {
-    fetch("http://localhost:3001/api/resources");
-  }, [])
-
   return (
     <Layout>
       <ResourceHighlight
@@ -28,7 +23,7 @@ function Home({resources}) {
 export async function getServerSideProps() {
   const resData = await fetch("http://localhost:3001/api/resources");
   const data = await resData.json();
-  console.log(data);
+  // console.log(data);
   return {
     props: {
       resources: data
