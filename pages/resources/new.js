13ccdad686ee1483c031213aa2fa94d6e7f06557
parampreet singh <parampreetsingh1092@gmail.com>
2023-10-15 13:33:21 +0530

    resource page

--- pages/resources/new.js
+++ pages/resources/new.js
@@ -1,11 +1,26 @@
 import Layout from "components/Layout"
 const ResourceCreate=()=>{
     return(
-        <Layout>
-          <div>
-            i am resource create page
+      <Layout>
+      <div className="container">
+        <div className="columns">
+          <div className="column is-8 is-offset-2">
+            <div className="resource-form">
+             <h1 className="title">Add New Resource</h1>
+              <form>
+               <div className="field">
+                <label className="label">Name</label>
+               <div className="control">
+                <input className="input" type="text" placeholder="Text input" />
+               </div>
+               </div>
+             </form>
+            </div>
           </div>
-        </Layout>
+        </div>
+      </div>
+    </Layout>
+    
     )
 }
 export default ResourceCreate;
