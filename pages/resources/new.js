c30a8c3fdb5ec8eaa397480f0914e7ca9a9eda1b
parampreet singh <parampreetsingh1092@gmail.com>
2023-10-16 16:01:39 +0530

    handling all changes

--- pages/resources/new.js
+++ pages/resources/new.js
@@ -6,8 +6,8 @@ const DEFAULT_DATA ={
   title: "",
   description: "",
   link: "",
-  priority: "2",
-  timeToFinish : "" 
+  priority: "1",
+  timeToFinish : "60" 
 }
 
 const ResourceCreate = () => {
@@ -16,12 +16,14 @@ const ResourceCreate = () => {
   const submitForm =()=>{
     alert(JSON.stringify(form));
   }
-  const handletitlechange = (e) =>{
+  const handlechange = (e) =>{
+    const { name,value }=e.target;
     setForm({
       ...form,
-      title: e.target.value
+      [name] :value
     })
   }
+  const resetForm=()=> setForm(DEFAULT_DATA);
   return (
     <Layout>
       <div className="container">
@@ -34,8 +36,9 @@ const ResourceCreate = () => {
                   <label className="label">Title</label>
                   <div className="control">
                   <input 
-                  onChange={handletitlechange}
+                  onChange={handlechange}
                   value={form.title} 
+                  name ="title"
                   className="input" 
                   type="text" 
                   placeholder="resource manager project using next js" />
@@ -45,20 +48,29 @@ const ResourceCreate = () => {
                 <div className="field">
                   <label className="label">Description</label>
                   <div className="control">
-                    <textarea value ={form.description} className="textarea" placeholder="important for placement pov"></textarea>
+                    <textarea value ={form.description} 
+                     onChange={handlechange}
+                     name="description"
+                    className="textarea" placeholder="important for placement pov"></textarea>
                   </div>
                 </div>
                 <div className="field">
                   <label className="label">Link</label>
                   <div className="control">
-                    <input value ={form.link}className="input" type="text" placeholder="https://resourcemanager.com" />
+                    <input value ={form.link}
+                    onChange={handlechange}
+                    name="link"
+                    className="input" type="text" placeholder="https://resourcemanager.com" />
                   </div>
                 </div>
                 <div className="field">
                   <label className="label">Priority</label>
                   <div className="control">
                     <div className="select">
-                      <select value ={form.priority}>
+                      <select value ={form.priority}
+                      onChange={handlechange}
+                      name="priority"
+                      >
                         <option>1</option>
                         <option>2</option>
                         <option>3</option>
@@ -69,7 +81,10 @@ const ResourceCreate = () => {
                 <div className="field">
                   <label className="label">Time to finish</label>
                   <div className="control">
-                    <input value ={form.timeToFinish}className="input" type="text" placeholder="" />
+                    <input value ={form.timeToFinish}
+                    onChange={handlechange}
+                    name ="timeToFinish"
+                    className="input" type="number" placeholder="" />
                   </div>
                   <p class="help">Time is in minutes</p>
                 </div>
@@ -81,7 +96,10 @@ const ResourceCreate = () => {
                   className="button is-link">Submit</button>
                 </div>
                 <div className="control">
-                   <button className="button is-link is-light">Cancel</button>
+                   <button 
+                   onClick={resetForm}
+                   type="button"
+                   className="button is-link is-light">Reset Form</button>
                 </div>
                 </div>
               </form>
