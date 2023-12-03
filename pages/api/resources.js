883ca6231b680fa8ac43eeaa288f5a1854aca298
parampreet singh <parampreetsingh1092@gmail.com>
2023-12-03 19:44:11 +0530

    commit

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,20 +1,25 @@
 
+import axios from "axios";
 export default async function(req, res) {
-  if(req.method == "GET"){
-
-
+  if (req.method === "GET") {
     const dataRes = await fetch("http://localhost:3001/api/resources");
     const data = await dataRes.json();
-
-     return res.send(data);
+    return res.send(data);
   }
-  if(req.method == "POST"){
-    console.log(req.body);
-    const { title,description,link,priority,timeToFinish} = req.body;
-    if(!title || !description || !link || !priority || !timeToFinish){
-     return res.status(422).send("Data is missing");
+  if (req.method === "POST") {
+    const { title, description, link, timeToFinish, priority} = req.body;
+    if (!title || !description || !link || !timeToFinish || !priority) {
+      return res.status(422).send("Data are missing!");
     }
-    res.send("Data has been recieved");
 
+    axios.post("http://localhost:3001/api/resources", req.body);
+
+    return res.send("Data has been received!");
+    try {
+      const axiosRes = await axios.post("http://localhost:3001/api/resources", req.body);
+      return res.send(axiosRes.data);
+    } catch {
+      return status(422).send("Data cannot be stored!");
+    }
   }
 }
